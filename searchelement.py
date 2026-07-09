6f1d28b813c592929cd3a638ccee28983142b620
Pudari Deepak <22wj8a6743@gniindia.org>
2026-07-09 23:28:37 +0530

    first commit

---
+++ searchelement.py
@@ -0,0 +1,22 @@
+# Search for a element in a left shifted sorted array and return the index
+def searchelement(arr,target):
+    low=0
+    high=len(arr)-1
+    while(low<=high):
+        mid = (low + high)//2
+        if (arr[mid] == target):
+            return mid
+        if(arr[low] <= arr[mid]):
+            if(target >= arr[low] and target <= arr[mid]):
+                high = mid-1
+            else:
+                low = mid+1
+        else:
+            if(target > arr[mid] and target <= arr[high]):
+                low = mid+1
+            else:
+                high = mid-1
+    return -1
+arr=[4,5,6,7,0,1,2]
+target=6
+print(searchelement(arr,target))
