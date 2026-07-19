46f3df34a8676fac2c6417e9183bb61a7046d176
Pudari Deepak <22wj8a6743@gniindia.org>
2026-07-19 20:05:17 +0530

    first commit

---
+++ validparentheses.py
@@ -0,0 +1,22 @@
+def isValid(s):
+    stack = []
+
+    pairs = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    }
+    for ch in s:
+        if ch in "({[":
+            stack.append(ch)
+        else:
+            if not stack:
+                return False
+            top = stack.pop()
+            if top != pairs[ch]:
+                return False
+    return len(stack) == 0
+
+
+s = "({[({[()]})]})"
+print(isValid(s))
